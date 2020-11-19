e1a27e8f8d428c5d8ee52cd86a2cc71f948b4af7
Rafa Prieto <rapribu@gmail.com>
2020-11-19 12:44:38 +0100

    Alias reducers

--- src/state/reducers/index.tsx
+++ src/state/reducers/index.tsx
@@ -1,10 +1,9 @@
-import { jobReducer } from './jobs'
-import { loaderReducer } from './loader'
+import { jobReducer as jobs } from './jobs'
+import { loaderReducer as loader } from './loader'
+import { bgColorReducer as bgColor } from './bgColor'
 import { combineReducers } from 'redux'
-import { bgColorReducer } from './bgColor'
-
 export const rootReducer = combineReducers({
-  jobReducer,
-  loaderReducer,
-  bgColorReducer
+  jobs,
+  loader,
+  bgColor
 })

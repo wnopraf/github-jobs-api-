73c755b2a87cf47b53d2701450020e587f782d88
Rafa Prieto <rapribu@gmail.com>
2020-11-19 12:11:01 +0100

    Add bgColor swithc action

--- src/state/actions/index.tsx
+++ src/state/actions/index.tsx
@@ -1,8 +1,10 @@
 import Axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { Action, Dispatch } from 'redux'
 
 import { ThunkAction } from 'redux-thunk'
-import { Job, SearchParams, LoadDataActionType } from '../../types'
-import { LOAD_DATA } from '../constants'
+import { Job, SearchParams, LoadDataAction, store, BgColor } from '../../types'
+import { IS_DARK, IS_LIGHT, LOAD_DATA } from '../constants'
 
 export const loadData = ({
   description,
@@ -11,12 +13,24 @@ export const loadData = ({
   Promise<void>,
   unknown,
   unknown,
-  LoadDataActionType
+  LoadDataAction
 > => async (dispatch, state) => {
   try {
-    const { data } = await Axios.get(
+    const { data }: { data: Job[] } = await Axios.get(
       `https://jobs.github.com/positions.json?description=${description}&location=${location}`
     )
     dispatch({ type: LOAD_DATA, payload: data })
   } catch (error) {}
+}
+
+const bgSwitchAction = () => {
+  const bgColorState = useSelector<store>((state) => state.bgColor) as BgColor
+  const dispatch = useDispatch<Dispatch>()
+
+  switch (bgColorState) {
+    case 'IS_DARK':
+      return dispatch({ type: IS_LIGHT })
+    case 'IS_LIGHT':
+      return dispatch({ type: IS_DARK })
+  }
 }

81ddb044864d4551ae4cc2993d845815f9f55a6d
Rafa Prieto <rapribu@gmail.com>
2020-11-25 10:27:34 +0100

    Add little fixes to padding

--- src/__fixtures__/SwitchBgButton.tsx
+++ src/__fixtures__/SwitchBgButton.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { Children, FunctionComponent } from 'react'
 import { Provider } from 'react-redux'
 import styled from 'styled-components'
-import { ToggleBgSwitch } from '../components/ToggleSwitch'
+import {
+  ToggleBgSwitch,
+  ToggleBgSwitchBarProps
+} from '../components/ToggleSwitch'
 import { store } from '../state'
-
-export default () => (
-  <Provider store={store}>
-    <FakeBgWrapper>
-      <ToggleBgSwitch />
-    </FakeBgWrapper>
-  </Provider>
-)
+import '../styles/index.css'
 
 const FakeBgWrapper = styled.div`
   background: rgb(141 152 234);
 `
+
+const TestProvider: FunctionComponent = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <FakeBgWrapper>{children}</FakeBgWrapper>
+    </Provider>
+  )
+}
+export default {
+  basic: (
+    <TestProvider>
+      <ToggleBgSwitch />
+    </TestProvider>
+  ),
+  medium: (
+    <TestProvider>
+      <ToggleBgSwitch barwidth={60} />
+    </TestProvider>
+  )
+}
--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,27 +1,111 @@
-import React, { FunctionComponent } from 'react'
-import styled from 'styled-components'
-import { useSelector, useDispatch, useStore } from 'react-redux'
-import { Store } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
+import React, {
+  EventHandler,
+  FunctionComponent,
+  ReactEventHandler,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from 'react'
+import styled, { StyledComponent } from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
+import { Dispatch } from 'redux'
 
-const ToggleBgSwitcherDiv = styled.div`
-  width: 50px;
+import { BgColor, BgColorAction, store } from '../types'
+import { IS_DARK, IS_LIGHT } from '../state/constants'
+import { bgSwitchAction } from '../state/actions'
+import { HiMoon, HiSun } from 'react-icons/hi'
+const ToggleBgSwitchBar = styled.div<{
+  barWidth: number
+  switchPos: BgColor
+}>`
+  width: ${({ barWidth }) => barWidth + 'px'};
   background: white;
-  border-radius: 50%;
-  .toggle-switcher--swithcer  {
-    witdth: 50px;
-    height: 50px;
+  border-radius: 1rem;
+  padding: ${({ barWidth }) => 0.1 * barWidth}px;
+  margin: 0 0.5rem;
+  cursor: pointer;
+  .toggle-switcher--switcher {
+    width: ${({ barWidth }) => barWidth * 0.3 + 'px'};
+    height: ${({ barWidth }) => barWidth * 0.3 + 'px'};
     border-radius: 50%;
-    background: purple;
-    cursor: pointer;
+    background: rgb(111 125 236);
+    transition: transform ease-in-out 300ms;
+    transform: ${({ switchPos, barWidth }) =>
+      switchPos === IS_LIGHT
+        ? 'translateX(0)'
+        : `translateX(${barWidth - barWidth * 0.3 - 0.1 * barWidth * 2}px)`};
+  }
+`
+export const ToggleBgSwitchBarProps: FunctionComponent<{
+  switchPos: BgColor
+  barWidth: number
+  switcherWidth: number
+}> = ({ barWidth = 30, switchPos }) => {
+  return (
+    <ToggleBgWrapper>
+      <ToggleBgSwitchBar barWidth={barWidth} switchPos={switchPos} />
+    </ToggleBgWrapper>
+  )
+}
+const ToggleBgWrapper = styled.div<{ barWidth?: number }>`
+  display: flex;
+  padding: 1rem 0;
+  align-items: center;
+  ${({ barWidth }) => ((barWidth as number) > 30 ? 'font-size: 1.6rem;' : '')}
+  .sun-icon,
+  .moon-icon {
+    color: white;
   }
 `
 
-export const ToggleBgSwitcher: FunctionComponent = () => {
-  const dispatch = useDispatch < ThunkDispatch<>()
+export const ToggleBgSwitch: FunctionComponent<{
+  barwidth?: number
+}> = ({ barwidth = 30 }) => {
+  const [switchState, setSwitchState] = useState<BgColor>('IS_LIGHT')
+
+  const switchStateRef = useRef<BgColor>('IS_LIGHT')
+  switchStateRef.current = switchState
+  const dispatch: Dispatch<BgColorAction> = useDispatch()
+  useEffect(() => {
+    const endTransitionSwithcHandler: ReactEventHandler = () => {
+      dispatch(bgSwitchAction(switchStateRef.current))
+    }
+
+    window.addEventListener('transitionend', endTransitionSwithcHandler as any)
+    return () => {
+      window.removeEventListener(
+        'transitionend',
+        endTransitionSwithcHandler as any
+      )
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log(bgState, 'bgState')
+  })
+
+  const bgState = useSelector<store, BgColor>((state) => state.bgColor)
+  const switchHandler: ReactEventHandler = () => {
+    switch (switchState) {
+      case IS_LIGHT:
+        return setSwitchState(IS_DARK)
+      case IS_DARK:
+        return setSwitchState(IS_LIGHT)
+    }
+  }
   return (
-    <ToggleBgSwitcherDiv>
-      <div className="toggle-switcher--switcher"></div>
-    </ToggleBgSwitcherDiv>
+    <ToggleBgWrapper barWidth={barwidth}>
+      <HiSun className="sun-icon" />
+      <ToggleBgSwitchBar
+        switchPos={switchState}
+        barWidth={barwidth}
+        onClick={switchHandler}
+      >
+        <div className="toggle-switcher--switcher"></div>
+      </ToggleBgSwitchBar>
+      <HiMoon className="moon-icon" />
+    </ToggleBgWrapper>
   )
 }

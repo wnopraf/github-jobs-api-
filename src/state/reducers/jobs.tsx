58305320dd2ab46ef603e2fab74496b8069ffe8e
Rafa Prieto <rapribu@gmail.com>
2020-12-16 12:23:35 +0100

    Replace bad type by jobDescription

--- src/state/reducers/jobs.tsx
+++ src/state/reducers/jobs.tsx
@@ -1,14 +1,14 @@
 import { Reducer } from 'redux'
-import { LoadDataAction, JobSearch } from '../../types'
+import { LoadDataAction, JobSearch, JobsDescription } from '../../types'
 import { LOAD_DATA } from '../constants'
 
-export const jobReducer: Reducer<JobSearch[], LoadDataAction> = (
+export const jobReducer: Reducer<JobsDescription[], LoadDataAction> = (
   state = [],
   { type, payload }
 ) => {
   switch (type) {
     case LOAD_DATA:
-      return [...payload] as JobSearch[]
+      return [...payload] 
 
     default:
       return state

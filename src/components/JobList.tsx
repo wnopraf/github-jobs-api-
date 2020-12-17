451f3579db23ab38dfd7b664224c513c34821844
Rafa Prieto <rapribu@gmail.com>
2020-12-17 19:59:03 +0100

    Add Link component and job items

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,17 +1,25 @@
 import React, { FunctionComponent, useEffect } from 'react'
+import { Link } from '@reach/router'
 import { useDispatch, useSelector } from 'react-redux'
 import { ThunkDispatch } from 'redux-thunk'
 import styled from 'styled-components'
 import { VscLoading } from 'react-icons/vsc'
 import { loadData } from '../state/actions'
-import { JobsDescription, JobSearch, LoadDataAction, Store } from '../types'
+import {
+  JobsDescription,
+  JobSearch,
+  LoadDataAction,
+  Loading,
+  Store
+} from '../types'
 import { mediaHelper } from '../lib/mediaUtil'
+import { JobItem } from './JobItem'
 
 export const AnimatedLoaderIcon = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   .loader-icon {
     @keyframes spinner {
@@ -29,30 +37,56 @@ export const AnimatedLoaderIcon = styled.div`
 `
 
 const StyledDiv$jobLists = styled.div`
+  margin-top: 10rem;
   display: grid;
-  grid-auto-flow: column;
+  grid-template-columns: 1fr;
+  grid-auto-rows: 230px;
+  grid-gap: 1rem 0;
   ${mediaHelper().tablet(`
-  grid-auto-column: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1rem;
+
+`)}
+  ${mediaHelper().desktop(`
+  grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 2rem;
 
 `)}
+a {
+    background: white;
+    border-radius: 0.45rem;
+    text-decoration: none;
+    padding: 1rem;
+    position: relative;
+    transition: box-shadow 200ms linear;
+    &:hover {
+      box-shadow: 0px 0px 10px 0px #0000003d;
+    }
+  }
 `
-export const JobLists: FunctionComponent<{ searchParams: JobSearch }> = ({
-  searchParams
-}) => {
+export const JobLists: FunctionComponent = () => {
   const thunkDispatch: ThunkDispatch<
     Store,
     unknown,
     LoadDataAction
   > = useDispatch()
-  const jobs = useSelector<Store>((store) => store.jobs)
-  const isLoading = useSelector<Store>((store) => store.loading)
+  const jobs = useSelector<Store, JobsDescription[]>((store) => store.jobs)
+  const isLoading = useSelector<Store, Loading>((store) => store.loader)
+  console.log(isLoading, 'is loaded')
 
   return isLoading ? (
     <AnimatedLoaderIcon>
       <VscLoading className="loader-icon" />
     </AnimatedLoaderIcon>
   ) : (
-    <div></div>
+    <StyledDiv$jobLists>
+      {jobs.map((el, i) => {
+        return (
+          <Link to={`/job/${el.id}`} key={i}>
+            <JobItem {...el} />
+          </Link>
+        )
+      })}
+    </StyledDiv$jobLists>
   )
 }

0692991bf8b352c44f46cc6ec9e3932b5afbe790
Rafa Prieto <rapribu@gmail.com>
2020-12-24 18:39:01 +0100

    Replace children by JobLists component

--- src/__fixtures__/PageMockList.tsx
+++ src/__fixtures__/PageMockList.tsx
@@ -4,18 +4,20 @@ import styled from 'styled-components'
 import { Container } from '../components/Container'
 import { Header } from '../components/Header'
 import { MobileJobsForm } from '../components/JobFilter'
+import { JobLists } from '../components/JobList'
 import { store } from '../state'
+
 const FakeBody = styled.div`
   background: rgb(234 234 234);
   padding-bottom: 5rem;
 `
-export default ({ children }) => (
+export default () => (
   <Provider store={store}>
     <FakeBody>
       <Header></Header>
       <Container>
         <MobileJobsForm />
-        {children}
+        <JobLists />
       </Container>
     </FakeBody>
   </Provider>

1439840f9a6720a2b4d0e47afd7ac03ab713819c
Rafa Prieto <rapribu@gmail.com>
2020-12-26 00:01:10 +0100

    Add golab styles to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
-
+import './styles/index.css'
 const root = document.querySelector('#root')
 
 render(<App />, root)

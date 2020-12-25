11e07ec81005da6928958faba8a9c6d27d9880ec
Rafa Prieto <rapribu@gmail.com>
2020-12-26 00:11:07 +0100

    Build app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,35 @@
-import React from 'react'
+import { Link, Router } from '@reach/router'
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import styled from 'styled-components'
+import { Container } from '../components/Container'
+import { Header } from '../components/Header'
+import { JobDetail } from '../components/JobDetail'
+import { MobileJobsForm } from '../components/JobFilter'
+import { store } from '../state'
+import { JobsDescription } from '../types'
+import { JobLists } from './JobList'
+const FakeBody = styled.div``
 
-export const App: React.FC = () => <div>Probando</div>
+const Home = () => (
+  <div className="home">
+    <Container>
+      <MobileJobsForm />
+      <JobLists />
+    </Container>
+  </div>
+)
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <Header></Header>
+
+      <Router>
+        <Home path="/" />
+
+        <JobDetail path="/job/:jobId" />
+      </Router>
+    </Provider>
+  )
+}
